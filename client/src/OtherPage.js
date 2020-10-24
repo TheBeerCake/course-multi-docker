cfd183390fb3f69996546e49ef7c5c217820a411
TheBeerCake <kristaps.klevers@gmail.com>
2020-10-24 20:55:59 +0300

    Add react client

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "reac-router-dom";
+
+export default () => {
+  retrun(
+    <div>
+      Some other page
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
